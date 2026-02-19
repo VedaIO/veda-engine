285a1fc6eb0534dcd078e8470f59c24619e19ac8
infraflakes <infraflakes@proton.me>
2026-02-19 20:24:57 +0700

    Remove wails components, fix receiver bugs, cover full APIs for IPC

--- src/internal/ipc/server.go
+++ src/internal/ipc/server.go
@@ -2,8 +2,13 @@ package ipc
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
 	"net"
+	"os"
+	"path/filepath"
+	"time"
+
 	"src/api"
 
 	"github.com/Microsoft/go-winio"
@@ -20,7 +25,6 @@ func NewServer(apiServer *api.Server) *Server {
 func (s *Server) Start() error {
 	address := GetIPCAddress()
 
-	// Use go-winio for Windows Named Pipes
 	config := &winio.PipeConfig{}
 	listener, err := winio.ListenPipe(address, config)
 	if err != nil {
@@ -64,13 +68,50 @@ func (s *Server) dispatch(req Request) Response {
 	var err error
 
 	switch req.Method {
+
+	// --- Stats ---
+
 	case "GetAppLeaderboard":
 		var params struct {
 			Since string `json:"since"`
 			Until string `json:"until"`
 		}
 		json.Unmarshal(req.Params, &params)
-		result, err = s.apiServer.Apps.GetAppLeaderboard(params.Since, params.Until)
+		result, err = s.apiServer.GetAppLeaderboard(params.Since, params.Until)
+
+	case "GetScreenTime":
+		result, err = s.apiServer.GetScreenTime()
+
+	case "GetTotalScreenTime":
+		result, err = s.apiServer.GetTotalScreenTime()
+
+	case "GetWebLeaderboard":
+		var params struct {
+			Since string `json:"since"`
+			Until string `json:"until"`
+		}
+		json.Unmarshal(req.Params, &params)
+		result, err = s.apiServer.GetWebLeaderboard(params.Since, params.Until)
+
+	case "Search":
+		var params struct {
+			Query string `json:"query"`
+			Since string `json:"since"`
+			Until string `json:"until"`
+		}
+		json.Unmarshal(req.Params, &params)
+		result, err = s.apiServer.Search(params.Query, params.Since, params.Until)
+
+	case "GetWebLogs":
+		var params struct {
+			Query string `json:"query"`
+			Since string `json:"since"`
+			Until string `json:"until"`
+		}
+		json.Unmarshal(req.Params, &params)
+		result, err = s.apiServer.GetWebLogs(params.Query, params.Since, params.Until)
+
+	// --- App Blocklist ---
 
 	case "GetAppBlocklist":
 		result, err = s.apiServer.GetAppBlocklist()
@@ -85,19 +126,109 @@ func (s *Server) dispatch(req Request) Response {
 		json.Unmarshal(req.Params, &names)
 		err = s.apiServer.UnblockApps(names)
 
-	case "GetScreenTime":
-		result, err = s.apiServer.Apps.GetScreenTime()
+	case "ClearAppBlocklist":
+		err = s.apiServer.ClearAppBlocklist()
 
-	case "GetTotalScreenTime":
-		result, err = s.apiServer.Apps.GetTotalScreenTime()
+	case "SaveAppBlocklist":
+		result, err = s.apiServer.SaveAppBlocklist()
 
-	case "CheckChromeExtension":
-		// TODO: Implement logic from bindings.go
-		result = false
+	case "LoadAppBlocklist":
+		var content []byte
+		json.Unmarshal(req.Params, &content)
+		err = s.apiServer.LoadAppBlocklist(content)
+
+	// --- Web Blocklist ---
+
+	case "GetWebBlocklist":
+		result, err = s.apiServer.GetWebBlocklist()
+
+	case "AddWebBlocklist":
+		var domain string
+		json.Unmarshal(req.Params, &domain)
+		err = s.apiServer.AddWebBlocklist(domain)
+
+	case "RemoveWebBlocklist":
+		var domain string
+		json.Unmarshal(req.Params, &domain)
+		err = s.apiServer.RemoveWebBlocklist(domain)
+
+	case "ClearWebBlocklist":
+		err = s.apiServer.ClearWebBlocklist()
+
+	case "SaveWebBlocklist":
+		result, err = s.apiServer.SaveWebBlocklist()
+
+	case "LoadWebBlocklist":
+		var content []byte
+		json.Unmarshal(req.Params, &content)
+		err = s.apiServer.LoadWebBlocklist(content)
+
+	// --- Auth ---
+
+	case "GetIsAuthenticated":
+		result = s.apiServer.GetIsAuthenticated()
+
+	case "Logout":
+		s.apiServer.Logout()
+		result = true
+
+	case "HasPassword":
+		result, err = s.apiServer.HasPassword()
+
+	case "Login":
+		var params struct {
+			Password string `json:"password"`
+		}
+		json.Unmarshal(req.Params, &params)
+		result, err = s.apiServer.Login(params.Password)
+
+	case "SetPassword":
+		var params struct {
+			Password string `json:"password"`
+		}
+		json.Unmarshal(req.Params, &params)
+		err = s.apiServer.SetPassword(params.Password)
+
+	// --- System ---
 
 	case "Shutdown":
 		s.apiServer.Shutdown()
 		result = true
+
+	case "Uninstall":
+		var params struct {
+			Password string `json:"password"`
+		}
+		json.Unmarshal(req.Params, &params)
+		err = s.apiServer.Uninstall(params.Password)
+
+	case "GetAutostartStatus":
+		result, err = s.apiServer.GetAutostartStatus()
+
+	case "EnableAutostart":
+		err = s.apiServer.EnableAutostart()
+
+	case "DisableAutostart":
+		err = s.apiServer.DisableAutostart()
+
+	case "ClearAppHistory":
+		var params struct {
+			Password string `json:"password"`
+		}
+		json.Unmarshal(req.Params, &params)
+		err = s.apiServer.ClearAppHistory(params.Password)
+
+	case "ClearWebHistory":
+		var params struct {
+			Password string `json:"password"`
+		}
+		json.Unmarshal(req.Params, &params)
+		err = s.apiServer.ClearWebHistory(params.Password)
+
+	// --- Local Checks ---
+
+	case "CheckChromeExtension":
+		result = checkChromeExtension()
 
 	default:
 		return Response{ID: req.ID, Error: "Unknown method: " + req.Method}
@@ -108,4 +239,21 @@ func (s *Server) dispatch(req Request) Response {
 	}
 
 	return Response{ID: req.ID, Result: result}
+}
+
+func checkChromeExtension() bool {
+	cacheDir, err := os.UserCacheDir()
+	if err != nil {
+		return false
+	}
+	heartbeatPath := filepath.Join(cacheDir, "Veda", "extension_heartbeat")
+	content, err := os.ReadFile(heartbeatPath)
+	if err != nil {
+		return false
+	}
+	var lastPing int64
+	if _, err := fmt.Sscanf(string(content), "%d", &lastPing); err != nil {
+		return false
+	}
+	return time.Since(time.Unix(lastPing, 0)) < 10*time.Second
 }
