21b69218d35690409391d6fd5ac5d265a9b4b826
infraflakes <infraflakes@proton.me>
2026-02-20 22:21:32 +0700

    Always start as SYSTEM

--- go.mod
+++ go.mod
@@ -5,7 +5,6 @@ go 1.25.5
 require (
 	github.com/Microsoft/go-winio v0.6.2
 	github.com/bi-zone/go-fileversion v1.0.0
-	github.com/shirou/gopsutil/v3 v3.24.5
 	go.mozilla.org/pkcs7 v0.9.0
 	golang.org/x/crypto v0.33.0
 	golang.org/x/sys v0.41.0
@@ -14,18 +13,10 @@ require (
 
 require (
 	github.com/dustin/go-humanize v1.0.1 // indirect
-	github.com/go-ole/go-ole v1.3.0 // indirect
 	github.com/google/uuid v1.6.0 // indirect
-	github.com/lufia/plan9stats v0.0.0-20211012122336-39d0f177ccd0 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/ncruces/go-strftime v0.1.9 // indirect
-	github.com/power-devops/perfstat v0.0.0-20210106213030-5aafc221ea8c // indirect
 	github.com/remyoudompheng/bigfft v0.0.0-20230129092748-24d4a6f8daec // indirect
-	github.com/shoenig/go-m1cpu v0.1.6 // indirect
-	github.com/stretchr/testify v1.10.0 // indirect
-	github.com/tklauser/go-sysconf v0.3.12 // indirect
-	github.com/tklauser/numcpus v0.6.1 // indirect
-	github.com/yusufpapurcu/wmi v1.2.4 // indirect
 	golang.org/x/exp v0.0.0-20250620022241-b7579e27df2b // indirect
 	golang.org/x/xerrors v0.0.0-20191204190536-9bdfabe68543 // indirect
 	modernc.org/libc v1.66.10 // indirect
--- src/main.go
+++ src/main.go
@@ -4,87 +4,15 @@ package main
 
 import (
 	"log"
-	"os"
-	"path/filepath"
-	"strings"
-	"veda-engine/src/api"
-	"veda-engine/src/internal/app/screentime"
-	"veda-engine/src/internal/data"
-	"veda-engine/src/internal/data/logger"
-	"veda-engine/src/internal/ipc"
-	"veda-engine/src/internal/monitoring"
-	"veda-engine/src/internal/platform/nativehost"
-	"veda-engine/src/internal/web/native_messaging"
 
 	"golang.org/x/sys/windows/svc"
 )
 
 func main() {
-	// Check if running as a Windows Service (started by SCM)
-	isService, err := svc.IsWindowsService()
+	// veda-engine runs exclusively as a Windows Service.
+	// It is registered and started by the veda launcher (veda.exe).
+	err := svc.Run("VedaEngine", &vedaService{})
 	if err != nil {
-		log.Fatalf("Failed to determine if running as service: %v", err)
+		log.Fatalf("Service failed: %v", err)
 	}
-
-	if isService {
-		runAsService()
-		return
-	}
-
-	// --- INTERACTIVE MODE (debug/dev or native messaging) ---
-
-	// Setup logging
-	cacheDir, _ := os.UserCacheDir()
-	logDir := filepath.Join(cacheDir, "Veda", "logs")
-	_ = os.MkdirAll(logDir, 0755)
-
-	logPath := filepath.Join(logDir, "Veda_engine.log")
-	logFile, _ := os.OpenFile(logPath, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0644)
-	if logFile != nil {
-		defer func() { _ = logFile.Close() }()
-		log.SetOutput(logFile)
-	}
-
-	log.Printf("=== VEDA ENGINE LAUNCHED (interactive) === Args: %v", os.Args)
-
-	// NATIVE MESSAGING HOST MODE
-	// Chrome launches us with the extension ID as an argument: chrome-extension://...
-	if len(os.Args) > 1 && strings.HasPrefix(os.Args[1], "chrome-extension://") {
-		log.Println("[MODE] Native Messaging Host detected")
-		native_messaging.Run()
-		log.Println("[MODE] Native Messaging Host exited")
-		return
-	}
-
-	// STANDALONE INTERACTIVE MODE (for debugging)
-	log.Println("[MODE] Interactive Standalone")
-
-	db, err := data.InitDB()
-	if err != nil {
-		log.Fatalf("Failed to initialize database: %v", err)
-	}
-
-	logger.NewLogger(db)
-	l := logger.GetLogger()
-	server := api.NewServer(db)
-
-	// Start background monitoring
-	monitoring.StartDefault(l, server.Apps, screentime.StartScreenTimeMonitor)
-
-	// Register Chrome extensions
-	if err := nativehost.RegisterExtension("hkanepohpflociaodcicmmfbdaohpceo"); err != nil {
-		log.Printf("Failed to register Store extension: %v", err)
-	}
-	if err := nativehost.RegisterExtension("gpaafgcbiejjpfdgmjglehboafdicdjb"); err != nil {
-		log.Printf("Failed to register Dev extension: %v", err)
-	}
-
-	// Start IPC Server (blocking)
-	ipcServer := ipc.NewServer(server)
-	log.Println("Veda Engine is starting IPC Server...")
-	if err := ipcServer.Start(); err != nil {
-		log.Fatalf("Failed to start IPC server: %v", err)
-	}
-
-	select {}
 }
